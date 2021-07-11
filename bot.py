b07083a0739619e8314f1ab180d2665a09430ea0
Rizal Widyananda <43237337+rizalw@users.noreply.github.com>
2021-07-11 17:01:49 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,13 @@ Token = "ODU1MzU3MTYzNzUyNzgzODgz.YMxTbw.0VPKrU9Pdt-h2PGjjfE1MywBgcw"
 @client.event
 async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='This App Grows'))
+    if not discord.opus.is_loaded():
+    # the 'opus' library here is opus.dll on windows
+    # or libopus.so on linux in the current directory
+    # you should replace this with the location the
+    # opus library is located in and with the proper filename.
+    # note that on windows this DLL is automatically provided for you
+        discord.opus.load_opus('opus')
     print("We have logged in as {}".format(client.user))
 
 @client.command()
